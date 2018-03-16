b9cbb820ee5bac81cbdcc2594f5b316c2bae8ac2
lutangar <johan.dufour@gmail.com>
2018-03-16 14:48:57 +0100

    add debug flag

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ require('isomorphic-fetch');
 module.exports = ({
     rpcscheme = 'http',
     rpchost = '127.0.0.1',
-    rpcport = 8332,
+    rpcport = 9998,
     rpcuser,
-    rpcpassword
+    rpcpassword,
+    debug = false,
 }) => new JsonRpcClient({
     endpoint: `${rpcscheme}://${rpchost}:${rpcport}`,
+    debug,
     headers: {
       'Authorization': `Basic ${new Buffer(rpcuser+':'+rpcpassword).toString('base64')}`,
     }
